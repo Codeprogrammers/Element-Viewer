11a156d30b7956c4196bd3fa0b8b6894eff029b4
Bradley Clemetson <brad@codeprogrammers.net>
2012-04-27 11:38:32 -0700

    Created initial empty Xcode template

---
+++ Element-ViewerTests/Element_ViewerTests.h
@@ -0,0 +1,13 @@
+//
+//  Element_ViewerTests.h
+//  Element-ViewerTests
+//
+//  Created by Bradley Clemetson on 4/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Element_ViewerTests : SenTestCase
+
+@end
---
+++ Element-ViewerTests/Element_ViewerTests.m
@@ -0,0 +1,32 @@
+//
+//  Element_ViewerTests.m
+//  Element-ViewerTests
+//
+//  Created by Bradley Clemetson on 4/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Element_ViewerTests.h"
+
+@implementation Element_ViewerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Element-ViewerTests");
+}
+
+@end
